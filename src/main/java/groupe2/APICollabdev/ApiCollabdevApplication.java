2e5bd6e67721525d7f7e29ac8e82a84a47f5f135
Djassi-Kone <150795696+Djassi-Kone@users.noreply.github.com>
2025-06-29 17:08:53 +0000

    la structure du projet

---
+++ src/main/java/groupe2/APICollabdev/ApiCollabdevApplication.java
@@ -0,0 +1,13 @@
+package groupe2.APICollabdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCollabdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCollabdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/groupe2/APICollabdev/ApiCollabdevApplicationTests.java
@@ -0,0 +1,13 @@
+package groupe2.APICollabdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCollabdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
